ef9fa2e73ded3f72155f5e33c68de86fbfe7bbff
JadedAlgorithms <algorfidelis247@gmail.com>
2025-06-05 23:25:08 +0530

    Using combinations to print pascal's triangle

---
+++ Week_1/leetcodebasics/pascalTriangle.py
@@ -0,0 +1,9 @@
+import math
+class Solution:
+    def generate(self, numRows: int) 
+        triangle=[]
+        for n in range(numRows):
+            row=[math.comb(n,r) for r in range(n+1)]
+            triangle.append(row)
+        return triangle
+#Here i used nCr (combinations) to find each element.
