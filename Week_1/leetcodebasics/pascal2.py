65b6c31e034f3e4beb6d6e21979929846befbe3e
JadedAlgorithms <algorfidelis247@gmail.com>
2025-06-08 05:17:34 +0530

    Finding the square root of a number

--- Week_1/leetcodebasics/pascal2.py
+++ Week_1/leetcodebasics/pascal2.py
@@ -1,5 +1,6 @@
+import math
 class Solution:
-    def getRow(self, rowIndex: int) -> List[int]:
+    def getRow(self, rowIndex: int):
         triangle=[]
         for n in range(rowIndex+1):
             row=[math.comb(n,r) for r in range(n+1)]
