4fde07ced20b4521b2938d7a111a81b3c04222b9
JadedAlgorithms <algorfidelis247@gmail.com>
2025-06-30 17:57:29 +0530

    Fixed error!

--- Week_1/leetcodebasics/Adding_Binary.py
+++ Week_1/leetcodebasics/Adding_Binary.py
@@ -1,7 +1,7 @@
 class Solution:
     def addBinary(self, a: str, b: str) -> str:
         decimal = int(a,2) + int(b,2)
-        if decimal == "0":
+        if decimal == 0:
             return "0"
         binary = ""
         while decimal >0:
